e06d054d0949b4a9b86cf38d1871f70ba99bbce7
Ian <ianbeltrao@gmail.com>
2022-10-04 10:55:31 +0100

    Step 11

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,3 +1,7 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
@@ -34,8 +38,10 @@ public class Arena {
         if(key.getKeyType() == KeyType.ArrowRight)  moveHero(hero.moveRight());
     }
 
-    public void draw(Screen screen) {
-        hero.draw(screen);
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#ffffff"));
+        graphics.fillRectangle(new TerminalPosition(0,0),new TerminalSize(width, height),' ');
+        hero.draw(graphics);
     }
 
     public int getHeight() {
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,6 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
@@ -10,7 +11,7 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-    private Screen screen;
+    private TerminalScreen screen;
 
     private Arena arena = new Arena(80, 40);
 
@@ -32,7 +33,7 @@ public class Game {
 
     private void draw() throws IOException {
         screen.clear();
-        arena.draw(screen);
+        arena.draw(screen.newTextGraphics());
         screen.refresh();
     }
 
